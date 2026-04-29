3c1c9553f372ca94466a95c6954c213fd74b4e14
oreoryza <moryzasativa@gmail.com>
2026-04-29 16:41:24 +0700

    change oauth

--- api/listings.js
+++ api/listings.js
@@ -1,12 +1,15 @@
 export default async function handler(req, res) {
   const tokenRes = await fetch('https://api.hostaway.com/v1/accessTokens', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      accountId: process.env.HOSTAWAY_ACCOUNT_ID,
-      apiKey: process.env.HOSTAWAY_API_KEY              
-    })
-  });
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: new URLSearchParams({
+    grant_type: 'client_credentials',
+    client_id: process.env.HOSTAWAY_ACCOUNT_ID,
+    client_secret: process.env.HOSTAWAY_API_KEY
+  })
+});
 
   const { accessToken } = await tokenRes.json();
 
