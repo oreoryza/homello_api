99eefc8853adcaaf40dcdd3dcf4f8946e0f1502c
oreoryza <moryzasativa@gmail.com>
2026-04-29 17:11:46 +0700

    change oauth back

--- api/listings.js
+++ api/listings.js
@@ -1,35 +1,25 @@
 export default async function handler(req, res) {
-  try {
-    const tokenRes = await fetch('https://api.hostaway.com/v1/accessTokens', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        accountId: process.env.HOSTAWAY_ACCOUNT_ID,
-        apiKey: process.env.HOSTAWAY_API_KEY
-      })
-    });
+  const tokenRes = await fetch('https://api.hostaway.com/v1/accessTokens', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: new URLSearchParams({
+    grant_type: 'client_credentials',
+    client_id: process.env.HOSTAWAY_ACCOUNT_ID,
+    client_secret: process.env.HOSTAWAY_API_KEY
+  })
+});
 
-    const tokenData = await tokenRes.json();
-    console.log("TOKEN RESPONSE:", tokenData);
+  const { accessToken } = await tokenRes.json();
 
-    if (!tokenData.accessToken) {
-      return res.status(400).json(tokenData);
+  const listingsRes = await fetch('https://api.hostaway.com/v1/listings', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
     }
+  });
 
-    const listingsRes = await fetch('https://api.hostaway.com/v1/listings', {
-      headers: {
-        Authorization: `Bearer ${tokenData.accessToken}`
-      }
-    });
+  const data = await listingsRes.json();
 
-    const data = await listingsRes.json();
-
-    res.status(200).json(data);
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
+  res.status(200).json(data);
 }
